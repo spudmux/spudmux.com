a7e90e422b110fe8b785f38eb49b29e01992402d
= <=>
2014-10-26 10:58:38 +0000

    clear rails site to do jekyll spike

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Spudmux::Application.config.secret_token = '94218b8951349bfe74f89e4d3c452eb0c386c484d72a6e32e35a8b1508f29d5ed45c4badfcee8d35f32e4aaab8f7feb4c747f1f8c8fe416f60fb4f78ab1aba73'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Spudmux::Application.config.session_store :cookie_store, :key => '_spudmux_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Spudmux::Application.config.session_store :active_record_store
